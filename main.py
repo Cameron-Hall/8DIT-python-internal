aee7ef9e2cc5eb6f1b662c47301e12bde742f571
Cameron Hall <22chall@wakatipu.school.nz>
2026-04-23 13:12:18 +1200

    Character class and basic movement
    Character can now move around and has a green trail everywhere they step. They are limited by the size of the grid and cannot leave it.

--- main.py
+++ main.py
@@ -2,40 +2,130 @@ from tkinter import *
 from tkinter import messagebox
 import random
 
+class Character:
+    def __init__(self, x, y, board):
+        self.x = x
+        self.y = y
+        self.board = board
+    
+    def move_up(self, board):
+        board.tilemap[self.y][self.x].configure(bg="green")
+        self.y -= 1
+        try:
+            if self.y < 0:
+                self.y = 0
+            self.update_character(board)
+        except IndexError:
+            self.y += 1
+            self.update_character(board)
+
+    def move_down(self, board):
+        board.tilemap[self.y][self.x].configure(bg="green")
+        self.y += 1
+        try:
+            self.update_character(board)
+        except IndexError:
+            self.y -= 1
+            self.update_character(board)
+
+    def move_right(self, board):
+        board.tilemap[self.y][self.x].configure(bg="green")
+        self.x += 1
+        try:
+            self.update_character(board)
+        except IndexError:
+            self.x -= 1
+            self.update_character(board)
+
+    def move_left(self, board):
+        board.tilemap[self.y][self.x].configure(bg="green")
+        self.x -= 1
+        try:
+            if self.x < 0:
+                self.x += 1
+            self.update_character(board)
+        except IndexError:
+            self.x += 1
+            self.update_character(board)
+        
+
+    def update_character(self, board):
+        board.tilemap[self.y][self.x].configure(bg="black")
+        self.print_pos()
+
+    def print_pos(self):
+        print(f"{self.x},{self.y}")
+
+
+class Box:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def detect_player(self, player):
+        pass
+
+    def move_up(self):
+        pass
+
+    def move_down(self):
+        pass
+
+    def move_left(self):
+        pass
+
+    def move_right(self):
+        pass
+
+
 class GUI:
     def __init__(self, parent):
         self.parent = parent
         self.parent.title("Sokoban")
 
-        self.seed = """10x10:ryyyybgbbbryyyybbbbbryyyybbbbbrbbbbyybbbrbbbbyybbbrbbbbbbbbbrbbbbbbbbbrbbbbbbbbbrbbbbbbbbbrbbbbbbbbb"""
+        self.seed = [["10x10:"],
+                     ["rrrrrrrrrr"],
+                     ["rwwwwwwwwr"],
+                     ["rwwwwwwwwr"],
+                     ["rwwwwwwwwr"],
+                     ["rrrrwwrrrr"],
+                     ["rwwwwwwwwr"],
+                     ["rwwrrrrwwr"],
+                     ["rwwwwwwwwr"],
+                     ["rrrwwwwrrr"],
+                     ["rrrrwwrrrr"]]
 
         self.TILE_SIZE = 25
 
-        self.WINDOW_WIDTH = int(self.seed[0:2])
-        self.WINDOW_HEIGHT = int(self.seed[3:5])
+        self.WINDOW_WIDTH = int(str(self.seed[0])[2:4])
+        self.WINDOW_HEIGHT = int(str(self.seed[0])[5:7])
         self.parent.geometry(f"{self.WINDOW_WIDTH*self.TILE_SIZE}x{self.WINDOW_HEIGHT*self.TILE_SIZE}+200+200")
 
-        if len(self.seed) != self.WINDOW_WIDTH*self.WINDOW_HEIGHT+6:
-            messagebox.showerror("bad code", "stinky code")
-            print(len(self.seed))
-            print(self.WINDOW_WIDTH*self.WINDOW_HEIGHT+6)
-        else:
-            messagebox.showinfo("great code", "amazing")
+        self.tilemap = []
 
         for i in range(self.WINDOW_HEIGHT):
+            self.tilemap.append([])
             for j in range(self.WINDOW_WIDTH):
-                color = self.seed[(i*self.WINDOW_HEIGHT)+j+6]
-                if color == "b":
-                    color = "blue"
-                elif color == "r":
+                color = str(self.seed[i+1])[j+2]
+                if color == "r":
                     color = "red"
-                elif color == "g":
-                    color = "green"
-                elif color == "y":
-                    color = "yellow"
+                elif color == "w":
+                    color = "white"
                 
                 f = Frame(self.parent, bg=color, width=self.TILE_SIZE, height=self.TILE_SIZE)
                 f.grid(row=i, column=j)
+                self.tilemap[i].append(f)
+
+        self.character = Character(5,1, self)
+
+        self.parent.bind("<KeyRelease-w>", lambda i: self.character.move_up(self))
+        self.parent.bind("<KeyRelease-s>", lambda i: self.character.move_down(self))
+        self.parent.bind("<KeyRelease-d>", lambda i: self.character.move_right(self))
+        self.parent.bind("<KeyRelease-a>", lambda i: self.character.move_left(self))
+
+        self.tilemap[self.character.y][self.character.x].configure(bg="black")
+
+                
 
 
 if __name__ == "__main__":
